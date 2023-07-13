e60fd22e8cb13c736e7eeb99d638886c3f80a795
siesto1elemento <rohitojha9720@gmail.com>
2023-07-14 00:52:50 +0530

    JS for the increment button

--- Passenger_counter/index.js
+++ Passenger_counter/index.js
@@ -1,1 +1,13 @@
-document.getElementById("count-el").innerText=6
+let countEl = document.getElementById("count-el")
+let count = 0
+
+function increment() {
+    count = count + 1
+    countEl.innerText = count
+}
+
+function save() {
+    console.log(count)
+}
+
+
